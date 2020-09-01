c5aa268bbbfc37c9e2a2aa11f07071472c52fdf9
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2020-08-31 22:03:11 -0300

    Adiciona as informacoes do filme no header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow'
 import './App.css';
 import FeaturedMovie from './components/FeaturedMovie'
-
 export default () => {
 
     const [movieList, setMovieList] = useState([]);
